edf673aed587c5844837055efb07764398201495
Sean Rachmani <sicsicrr11@gmail.com>
2026-04-17 14:50:48 +0300

    part 2 -done

--- src/part2.ts
+++ src/part2.ts
@@ -6,20 +6,20 @@ const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-/*helper function*/
+/*helper function:*/
 //takes a char and return trun if vowels array includes it/
 const isVowel: (s: string) => boolean = (s: string) => R.includes(s,vowels);
-/*actual function*/
+/*actual function:*/
 export const countVowels: (s: string) => number = R.pipe(R.toLower,stringToArray,R.filter(isVowel),R.length);
   
     
 
 /* Question 2.2 */
-/*helper function1*/
+/*helper function:*/
 //removes spaces, punctuation, and capitalization
 //pipe returns function that takes s 
 const cleanString: (s: string) => string = R.pipe(R.toLower,R.replace(/[^a-z0-9]/g,""));
-/*actual function*/
+/*actual function:*/
 export const isPalindrome: (text: string) => boolean =(text: string) =>{
     const clean: string = cleanString(text);
     const reversed: string = R.reverse(clean);
@@ -33,4 +33,8 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+export const treeToSentence: (t: WordTree) => string = (t: WordTree) => {
+    //a new array with the words we need to concat:
+    const array: string[] =R.prepend(t.root,R.map(treeToSentence,t.children));
+    return R.join(" ",array);
+};
