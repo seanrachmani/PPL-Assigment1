f9401ca08eca82c177fd9d14792c112bb8b3adec
Sean Rachmani <sicsicrr11@gmail.com>
2026-04-17 13:53:48 +0300

    2.2

--- src/part2.ts
+++ src/part2.ts
@@ -8,16 +8,23 @@ const stringToArray = R.split("");
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
 /*helper function*/
 //takes a char and return trun if vowels array includes it/
-const isVowel = (s: string) : boolean => R.includes(s,vowels);
-
+const isVowel: (s: string) => boolean = (s: string) => R.includes(s,vowels);
 /*actual function*/
-export const countVowels: (s: string) => number = 
-    R.pipe(R.toLower,stringToArray,R.filter(isVowel),R.length);
+export const countVowels: (s: string) => number = R.pipe(R.toLower,stringToArray,R.filter(isVowel),R.length);
   
     
-    
+
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
+/*helper function1*/
+//removes spaces, punctuation, and capitalization
+//pipe returns function that takes s 
+const cleanString: (s: string) => string = R.pipe(R.toLower,R.replace(/[^a-z0-9]/g,""));
+/*actual function*/
+export const isPalindrome: (text: string) => boolean =(text: string) =>{
+    const clean: string = cleanString(text);
+    const reversed: string = R.reverse(clean);
+    return R.equals(clean,reversed);
+};
   
 
 /* Question 2.3 */
