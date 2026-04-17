4d2c00f43a318465ca72d1f58452549641fad510
Sean Rachmani <sicsicrr11@gmail.com>
2026-04-17 22:30:50 +0300

    part3 finishim

--- src/part3.ts
+++ src/part3.ts
@@ -11,10 +11,11 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
 //3.1:
 //takes a predicate and an array
 //returns an Ok on the first element that the predicate returns true for, or a Failure if no such element exists.
-export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => {
-    const filtered: T[] = a.filter(pred);
-    return filtered.length === 0 ? makeFailure("no such elemnt exists") : makeOk(filtered[0]);
-};
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> =>
+     a.length === 0 ? makeFailure("No element found.") :
+     pred(a[0]) === true ? makeOk(a[0]) : 
+     findResult(pred,a.slice(1));
+
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -30,17 +31,16 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
 //Only using bind
 //uses findResult
 //return an Ok on the first even value squared, or a Failure if no even numbers exist.
-export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => {
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => 
     //bind takes original monad anf square func
     //find result takes predicate&array
-    return bind(findResult((x: number) => x % 2 === 0, a),(x: number) => makeOk(x*x));
-};
+    bind(findResult((x: number) => x % 2 === 0, a),(x: number) => makeOk(x*x));
 
 //3.3
 //Only using either 
 //uses findResult
 //return the first even value squared, or a -1 if no even numbers exist.
-export const returnSquaredIfFoundEven_v3 = (a: number[]): number => {
-    return either(findResult((x: number) => x % 2 === 0,a), (value: number) => value * value, (message: string) => -1);
-}
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number => 
+    either(findResult((x: number): boolean => x % 2 === 0,a), (value: number): number => value * value, (message: string): number => -1);
+
 
